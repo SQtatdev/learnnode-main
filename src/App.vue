17b1971fd422ad2355ee441001f65521faddf0f5
sqtatdev <slava.ryvlin@gmail.com>
2024-12-09 11:04:51 +0200

    update2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-let message = 'Hello Vue!'
+import {ref} from 'vue';
+let message = ref('Hello Vue!');
+let isReversed = ref(true)
 </script>
 
 <template>
-    <div class="conteiner">
+    <div class="conteiner mt-3">
         <div class="content">
-            <h1>{{ message }}</h1>
+            <h1 v-if="isReversed">{{ message.split('').reverse().join('') }}</h1>
+            <h1 v-else>{{ message }}</h1>
+           
+             <input class="input my-1" v-model="message" > <!-- v-bind:value="message" @input="message=$event.target.value -->
+            <button class="button is-primary" @click="message='Hello Ta23B'" > Click me </button>
+            <button class="button is-primary mx-4" @click="isReversed=!isReversed" > reversse again </button>
         </div>
     </div>
 </template>
